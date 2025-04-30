de00a01fc9c7157c889c06ea148c034fbec204e1
MaguRomera <maguromera@gmail.com>
2025-04-30 16:16:53 -0300

    Styles del header de "detalleArtista"

--- src/components/ArtistDetailsHeader.jsx
+++ src/components/ArtistDetailsHeader.jsx
@@ -1,20 +1,10 @@
 export function ArtistDetails(props){
-    function handleSongs() {
-        axios
-          .get(`https://api.spotify.com/v1/artists/${id}/songs`)
-          .then((data) => {
-            console.log(data.data) //SACAR ANTES DE DEPLOYAR
-            setArtistInfo(data.data)
-          })
-          .catch((error) => {
-            console.log("Error!:", error)
-          })
-    }
+        
     return(
         <section className='artistinfo-cnt'> 
             <img src={props.artistInfo.images[0]?.url} alt="Artist image" className='artistinfo-img'/> 
             <div className='text-cnt'>
-                <h3>{props.artistInfo.name}</h3>
+                <h3>{props.artistInfo.name.toUpperCase()}</h3>
                 <p>{props.artistInfo.followers.total} followers</p> 
             </div>
         </section>
